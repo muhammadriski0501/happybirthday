f3f5db84e60289303ae6ae4e1a8566bd21ff4570
muhammadriski0501 <55203199+muhammadriski0501@users.noreply.github.com>
2019-09-19 09:02:48 +0700

    Initial commit

---
+++ app/src/main/java/com/example/iriz_techno/helloword/helloword.java
@@ -0,0 +1,13 @@
+package com.example.iriz_techno.heppybirthday;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class happybirthday extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_happybirthday);
+    }
+}
